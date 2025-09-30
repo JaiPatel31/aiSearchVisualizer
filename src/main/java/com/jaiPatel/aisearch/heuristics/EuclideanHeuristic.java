5a5d255415365ded9995e1d2e7ca2ff1d74ac5ea
JaiPatel31 <149627486+JaiPatel31@users.noreply.github.com>
2025-09-30 17:45:48 -0500

    Allowed Nodes to be double and reflected those changes to heuristics

--- src/main/java/com/jaiPatel/aisearch/heuristics/EuclideanHeuristic.java
+++ src/main/java/com/jaiPatel/aisearch/heuristics/EuclideanHeuristic.java
@@ -5,8 +5,8 @@ import com.jaiPatel.aisearch.graph.Node;
 public class EuclideanHeuristic implements Heuristic {
     @Override
     public double estimate(Node current, Node goal) {
-        int dx = current.getX() - goal.getX();
-        int dy = current.getY() - goal.getY();
+        double dx = current.getX() - goal.getX();
+        double dy = current.getY() - goal.getY();
         return Math.sqrt(dx * dx + dy * dy);
     }
 }
