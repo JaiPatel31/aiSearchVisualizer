df8e563a60d4aaba31a8f2bffb7c8e4627ec8761
Jai Patel <jaipatel4717@gmail.com>
2025-09-30 11:08:27 -0500

    Added BFS class

---
+++ src/main/java/com/jaiPatel/aisearch/algorithms/BFS.java
@@ -0,0 +1,11 @@
+package com.jaiPatel.aisearch.algorithms;
+
+import com.jaiPatel.aisearch.graph.*;
+
+public class BFS implements SearchAlgorithm{
+    @Override
+    public SearchResult solve(Graph graph, Node start, Node goal) {
+        return null;
+    }
+
+}
